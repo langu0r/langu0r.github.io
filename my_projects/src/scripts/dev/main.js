44ae3460181e148b8a8b0c24393f94ca8e66b7ae
langu0r <khlksenia09@gmail.com>
2025-02-20 00:44:48 +0700

    footer, нормализация, jquery, валидация форм, favicons

--- my_projects/src/scripts/dev/main.js
+++ my_projects/src/scripts/dev/main.js
@@ -288,5 +288,77 @@ if (contactsMap) {
 }
 
 
+const jsSelectric = $(".js-selectric");
+if (jsSelectric.length) {
+  jsSelectric.selectric({
+    nativeOnMobile: false
+  });
+}
+
+const mobileMask = $('.js-mobileMask');
+if (mobileMask.length) {
+  mobileMask.mask('+7 (000) 000 00 00', { placeholder: "+7 (___) ___ __ __" });
+}
+
+
+const dateField = $(".js-dateField");
+if (dateField.length) {
+  const pickerInit = function (pick) {
+    const dateInput = pick.find(".js-dateInput");
+    const dateDay = pick.find(".js-dateDay");
+    const dateMonth = pick.find(".js-dateMonth");
+    const dateYear = pick.find(".js-dateYear");
+    const dateConfig = {
+      autoClose: true,
+      minDate: new Date(),
+      navTitles: {
+        days: "MMMM <i>yyyy</i>"
+      },
+      onSelect: function ({ date }) {
+        dateDay.val(date ? ("0" + date.getDate()).slice(-2) : "");
+        dateMonth.val(date ? ("0" + (date.getMonth() + 1)).slice(-2) : "");
+        dateYear.val(date ? date.getFullYear() : "");
+      }
+    };
+    new AirDatepicker(dateInput[0], dateConfig);
+  };
+  $.each(dateField, function (i) {
+    pickerInit($(this));
+  });
+}
+
+const eventForm = $('#js-eventForm');
+if (eventForm.length) {
+  eventForm.validate({
+    errorElement: "span"
+  });
+}
+
+const subscribeForm = $("#js-subscribeForm");
+if (subscribeForm.length) {
+  const subscribeAction = subscribeForm.attr("action");
+  const subscribeEmail = subscribeForm.find("#js-subscribeEmail");
+  subscribeForm.validate({
+    errorElement: "span",
+    submitHandler: function (form, event) {
+      event.preventDefault();
+      $.ajax({
+        url: subscribeAction,
+        method: "POST",
+        data: {
+          email: subscribeEmail.val()
+        },
+        success: function () {
+          subscribeEmail.val("");
+          subscribeEmail.blur();
+          alert("Вы успешно подписались на рассылку новостей");
+        },
+        error: function () {
+          alert("Что-то пошло не так, попробуйте еще раз");
+        }
+      });
+    }
+  });
+}
 
   })();
